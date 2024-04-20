6668233f851827d5232431161ec4c3cb3bb7263e
JhonDCT <deyner.catacora@gmail.com>
2024-04-20 17:44:02 -0500

    change package name

--- stencil.config.ts
+++ stencil.config.ts
@@ -20,12 +20,7 @@ export const config: Config = {
       type: 'www',
       serviceWorker: null, // disable service workers
     },
-    angularOutputTarget({
-      componentCorePackage: 'stencil-library',
-      outputType: 'component',
-      directivesProxyFile: '../angular-workspace/projects/component-library/src/lib/stencil-generated/components.ts',
-      directivesArrayFile: '../angular-workspace/projects/component-library/src/lib/stencil-generated/index.ts',
-    })
+   
   ],
   testing: {
     browserHeadless: "new",
